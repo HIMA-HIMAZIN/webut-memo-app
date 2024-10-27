f6d66108388bb55eff908442326ed67bd1082a1f
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-27 23:51:36 +0900

    ボタン背景の変更

--- src/components/sidebars/LeftSideBar/index.tsx
+++ src/components/sidebars/LeftSideBar/index.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import ClubIcon from "@icons/club.svg";
-import ActionButton  from "@/components/buttons/ActionButton";
+import {ActionButton}  from "@/components/buttons/ActionButton";
 
 export function LeftSideBar() {
   return (
-    <div className="bg-zinc-100 h-screen w-1/6">
-      <div className="flex items-start justify-center h-16 border-b border-gray-300">
-        <ClubIcon />
-      </div>
+    <div className="bg-zinc-100 h-screen w-1/6"
+    style={{
+      backgroundColor:"#FAFAFA",
+      }}>
+      
       <div>
-        <ActionButton title="ホーム" iconPath="/icons/home.svg" path="/" />
-        <ActionButton title="マイページ" iconPath="/icons/user.svg" path="/mypage" />
-        <ActionButton title="設定" iconPath="/icons/settings.svg" path="/settings" />
-        
+        <ActionButton title="マイページ"path="/" />
+        <ActionButton title="設定" path="/settings" />
       </div>
     </div>
   );
