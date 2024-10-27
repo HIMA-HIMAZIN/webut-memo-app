e946258e21993111456dffd278631a1476b9cd71
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-27 23:51:21 +0900

    リンクの変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {LeftSideBar} from './components/sidebars/LeftSideBar';
-import {RightSideBar} from './components/sidebars/RightSideBar';
+import {LeftSideBar} from '../components/sidebars/LeftSideBar';
+import {RightSideBar} from '../components/sidebars/RightSideBar';
 export default function Home() {
   return (
     <div className="flex items-center justify-between">
