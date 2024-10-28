d002a119259d11fdc245fb87487377bdf16e734b
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-29 03:21:35 +0900

    モーダルの開閉の処理記述

--- src/components/sidebars/LeftSideBar/index.tsx
+++ src/components/sidebars/LeftSideBar/index.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import {ActionButton}  from "@/components/buttons/ActionButton";
-import {ProfileButton} from "@/components/buttons/ProfileButton";
-import { Settings,Planet } from 'iconoir-react';
+import React, { useState } from "react";
+import { Settings,Planet, Edit } from 'iconoir-react';
 import Image from 'next/image';
 
+// components
+import {ActionButton}  from "@/components/buttons/ActionButton";
+import {ProfileButton} from "@/components/buttons/ProfileButton";
+import {PrimaryButton} from "@/components/buttons/PrimaryButton";
+import { MemoModal } from "@/components/modals/MemoModal";
+
 export function LeftSideBar() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
   return (
     <div className="bg-zinc-100 h-screen w-1/5 pl-5 pt-5"
     style={{
@@ -23,6 +37,8 @@ export function LeftSideBar() {
         <ProfileButton title="くまさん" path="/" />
         <ActionButton title="みんな"path="/" icon={Planet}/>
         <ActionButton title="設定" path="/settings" icon={Settings}/>
+        <PrimaryButton title="メモする" icon={Edit} onClick={handleButtonClick}/>
+        <MemoModal isOpen={isModalOpen} onClose={handleCloseModal} />
       </div>
     </div>
   );
