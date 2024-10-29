9d2920737ba09531b0090d73955864c414d7ddda
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-10-29 18:11:02 +0900

    高さ調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {LeftSideBar} from '../components/sidebars/LeftSideBar';
-import {RightSideBar} from '../components/sidebars/RightSideBar';
+import { LeftSideBar } from '../components/sidebars/LeftSideBar';
+import { RightSideBar } from '../components/sidebars/RightSideBar';
+
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between h-screen">
       <LeftSideBar />
       <RightSideBar />
     </div>
