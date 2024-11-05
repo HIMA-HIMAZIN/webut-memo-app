3adeb66253d08c293e3ab733963f6f1bf7792983
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-05 17:44:05 +0900

    sidebarではなく、pageでのモーダル表示に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import { LeftSideBar } from '../components/sidebars/LeftSideBar';
 import ReloadButton from '../components/buttons/ReloadButton';
+import { MemoModal } from '@/components/modals/MemoModal';
 
 export default function Home() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="flex justify-center min-h-screen bg-contentbg">
       <div className="flex w-full max-w-7xl">
         
         <div className="hidden md:block w-1/4 bg-contentbg p-4">
-          <LeftSideBar />
+          <LeftSideBar onOpenModal={handleOpenModal} />
         </div>
-        <div className="w-full md:w-1/2  bg-white">
+        
+        <div className="w-full md:w-1/2 bg-white">
           <ReloadButton />
         </div>
+        
         <div className="hidden md:block w-1/4 bg-contentbg p-4">
           右サイド
         </div>
       </div>
+
+      {/* モーダルをルート直下に配置し、常に表示できるようにする */}
+      <MemoModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 }
