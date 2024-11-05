ee2b01487dc8fb38b3aeb4325f504196b17792e1
Shvain <takuto3987@gmail.com>
2024-11-05 15:30:22 +0900

    feat：テーマの追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        contentbg: "#FAFAFA",
+        action: {
+          DEFAULT: "#007AFF",
+        },
+        primary: {
+          DEFAULT: "#5DB53E",
+          hover: "#46A325",
+        },
       },
     },
   },
