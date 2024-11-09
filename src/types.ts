2c59e42b1edde9626822a2cff7639146cae2e75c
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-09 14:52:19 +0900

    supabase.tsの作成

--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,10 @@
 // メモの型
 export type MemoLogType = {
     id: number;
+    user_id: string;
     content: string;
     is_public: boolean;
+    like_count: number;
     created_at: string;
     updated_at: string;
-    user_id: string;
 };
