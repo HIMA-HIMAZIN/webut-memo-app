4ddad1aec82a8bf8898901c4124b95565fe68682
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-11 02:05:18 +0900

    icon_numberを受け取って使用するように変更

--- src/components/buttons/ProfileButton/index.tsx
+++ src/components/buttons/ProfileButton/index.tsx
@@ -4,26 +4,28 @@
 import Link from "next/link";
 import React from 'react';
 import Image from 'next/image';
+import {getImageSrcById} from '@/utils/iconImage/getImageSrcById';
 
 interface ProfileButtonProps {
   title: string;
   path: string;             
   hideTextOnSmallScreen?: boolean; 
+  icon_number: number;
 }
 
 export function ProfileButton({
   title,
   path,
   hideTextOnSmallScreen = false,
+  icon_number
 }: ProfileButtonProps) {
-  const imageNumber = path.split('/').pop(); // pathの末尾の番号を取得
-  const imageUrl = `/images/profile_icon/${imageNumber || 'panda'}.png`; // プレースホルダー画像も設定
+  const icon_url = getImageSrcById(icon_number);
   return (
     <Link href={`/${path}`}>
       <button
       className={`flex justify-start hover:bg-gray-100 items-center m-3 mb-1 text-black font-bold rounded-full w-fit`}
     >
-      <Image className='rounded-full' src={imageUrl} alt="profile" height={40} width={40} />
+      <Image className='rounded-full' src={icon_url} alt="profile" height={40} width={40} />
       <div className={`ml-2 text-2xl ${hideTextOnSmallScreen ? 'hidden lg:block' : 'block'} whitespace-nowrap mx-2`}>
         {title}
       </div>
