3ce775a2d11c82f2d88e83b866aa6c23ec149ee9
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-11 17:59:04 +0900

    修正

--- src/filters/profanityFilter.ts
+++ src/filters/profanityFilter.ts
@@ -2,6 +2,6 @@ import { forbiddenWords } from "./japaneseWords";
 
 export const filterProfanity = (text: string): boolean => {
   // 禁止ワードを含むかをチェック
-  const regex = new RegExp(`(?:^|\\s|[.,;!?])(${forbiddenWords.join("|")})(?=\\s|$|[.,;!?])`, "i");
+  const regex = new RegExp(forbiddenWords.join("|"), "i");
   return regex.test(text);
 };
