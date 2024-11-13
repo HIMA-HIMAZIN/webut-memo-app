a0e30040c9d4dc3e7791307b20e7ccd733947fd9
Koutah15 <kiruka1212@gmail.com>
2024-11-13 11:08:47 +0900

    eslintルール変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-// .eslintrc.js
 module.exports = {
     env: {
       browser: true,
@@ -9,10 +8,16 @@ module.exports = {
       "plugin:react/recommended",
       "plugin:@typescript-eslint/recommended",
       "plugin:next/recommended", 
+      "next/core-web-vitals", // Next.js 推奨設定を追加
     ],
     parserOptions: {
       ecmaVersion: 12,
       sourceType: "module",
+    },
+    settings: {
+      react: {
+        version: "detect", // React バージョンを自動検出
+      },
     },
     rules: {
       "indent": ["error", 2],
@@ -21,7 +26,11 @@ module.exports = {
       "no-unused-vars": "warn",
       "no-console": "warn",
       "eqeqeq": ["error", "always"],
+      "react/react-in-jsx-scope": "off", // React 17+ では不要
+      "react/prop-types": "off", // TypeScript を使用する場合は無効化
+      "no-empty-pattern": "error", // 空のオブジェクトパターンをエラーとして設定
+      "no-irregular-whitespace": "error", // 不規則な空白をエラーとして設定
       "@next/next/no-html-link-for-pages": "off", 
     },
-  }
+  };
   
